4754d6172316c5ba2cc61a251888f7b153df70ff
Ifeoulwah <spencerife.21@gmail.com>
2022-10-16 11:08:28 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.scss";
+import LogoutIcon from "./components/UI/Icons/LogoutIcon";
+
+const App = () => {
+  return (
+    <div>
+      <LogoutIcon />
+    </div>
+  );
+};
+
+export default App;
